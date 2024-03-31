16c5e9efd40394e61097f67ab9a6819f020ab8d5
ANIKIN <abn.dev.01@gmail.com>
2024-03-31 04:14:45 +0300

    add bank card demo

--- number-converter/src/test/java/pro/abnjava/jvm/number/converter/HowToUseDemo.java
+++ number-converter/src/test/java/pro/abnjava/jvm/number/converter/HowToUseDemo.java
@@ -4,9 +4,20 @@ import java.util.Optional;
 
 public class HowToUseDemo {
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         InputChecker checker = new StandardInputChecker();
         String input = "5590 9999 0000 9999";
+        Optional<ResultType> result = checker.checkInput(input);
+
+        result.ifPresentOrElse(
+            r -> System.out.println("Valid input processed. Result: " + r),
+            () -> System.out.println("Invalid input.")
+        );
+    }
+
+    public static void main(String[] args) {
+        InputChecker checker = new StandardInputChecker();
+        String input = "4532 7562 7962 4064";
         Optional<ResultType> result = checker.checkInput(input);
 
         result.ifPresentOrElse(
